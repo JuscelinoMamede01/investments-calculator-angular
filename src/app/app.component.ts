8d33163ad3469370181b407eea9aa2df28415967
JuscelinoMamede <juscemamede01@gmail.com>
2025-03-30 11:11:37 -0300

    jk: creating two-way-biding at form calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
